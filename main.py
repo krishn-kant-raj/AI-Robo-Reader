e6410bff5b6a129d625e0e103d8da4c4b5a8220f
Krishn Kant Raj <krishnkantraj@gmail.com>
2021-01-03 19:08:35 +0530

    Documentation added, start and end page number bug fixed

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 import cv2
 
 def image_to_text_reader(img_path):
+    """
+    :param img_path: image path
+    :return: text
+
+    take full path of an image and extract the text on it
+    return the text
+    """
     img = cv2.imread(img_path)
     text = tess.image_to_string(img)
-    print(text)
     return text
 
 def text_to_speech(text, gender, v_rate):
@@ -15,6 +21,7 @@ def text_to_speech(text, gender, v_rate):
     Function to convert text to speech
     :param text: text
     :param gender: gender
+    :param v_rate: speech rate
     :return: None
     """
     voice_dict = {'Male': 0, 'Female': 1}
@@ -32,11 +39,19 @@ def text_to_speech(text, gender, v_rate):
     # Change voices: 0 for male and 1 for female
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[code].id)
-
     engine.say(text)
     engine.runAndWait()
 
 def read_text_from_pdf(fname,s_pgnum, e_pgnum):
+    """
+    :param fname: file path with extension
+    :param s_pgnum: starting page number
+    :param e_pgnum: End of page number
+    :return: text
+
+    If any error occurs then
+    :return: enter or past your file path with proper extension
+    """
     try:
         book = open(fname, 'rb')
         pdfReader = PyPDF2.PdfFileReader(book)
@@ -46,15 +61,15 @@ def read_text_from_pdf(fname,s_pgnum, e_pgnum):
         elif s_pgnum > pages:
             s_pgnum = 0
         elif s_pgnum > 1 and s_pgnum < pages:
-            s_pgnum = s_pgnum + 1
+            s_pgnum = s_pgnum - 1
         else:
             s_pgnum = 0
 
-        if e_pgnum > pages:
+        if e_pgnum > pages or e_pgnum == 0:
             e_pgnum = pages
         elif e_pgnum == s_pgnum:
             e_pgnum = pages
-            s_pgnum = s_pgnum - 1
+            s_pgnum = 0
 
         for num in range(s_pgnum, e_pgnum):
             page = pdfReader.getPage(num)
@@ -65,17 +80,15 @@ def read_text_from_pdf(fname,s_pgnum, e_pgnum):
 
     except (FileNotFoundError,PyPDF2.utils.PdfReadError,TypeError):
         return 'enter or past your file path with proper extension'
-    finally:
-        return 'enter or past your file path with proper extension'
 
 s_pgnum = 0
 e_pgnum = 1
 fname = ""
 img_path = ''
 try:
-    text = read_text_from_pdf(fname,s_pgnum,e_pgnum)
+    text = read_text_from_pdf(fname, s_pgnum, e_pgnum)
 except FileNotFoundError:
-    text=' '
+    text = ' '
 except PyPDF2.utils.PdfReadError:
     text = 'This is not a PDF file.'
 
